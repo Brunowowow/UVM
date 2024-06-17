b18e0bcba9de1187f8d3c4dc60d6b315abd8baa2
Brunowowow <brunitoqlbacan@gmail.com>
2024-06-17 19:14:32 -0400

    Add files via upload

---
+++ RamenDeIchiraku.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ramendeichiraku;
+
+import Controlador.Registro_Usuario;
+import Modelo.Usuario;
+import Vista.Login;
+
+/**
+ *
+ * @author matiasguerra
+ */
+public class RamenDeIchiraku {
+
+    public static void main(String[] args) {
+        
+        Login login = new Login ();
+        login.setVisible(true);
+    }
+}
